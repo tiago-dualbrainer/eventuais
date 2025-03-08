7e770cf8a4504542409b135f9a1589a75a7cef3a
tiago-tipple <tiago@heytipple.com>
2025-03-08 16:11:44 +0000

    first eventuais commit

---
+++ eventuais/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from eventuais.users.models import User
+from eventuais.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
